990487748376f6d92b6567493a4c62a784d5e154
Ben Thomasson <bthomass@redhat.com>
2022-03-05 09:38:57 -0500

    Adds fact prefix to identifiers

---
+++ scripts/run-github-deploy-tests.sh
@@ -0,0 +1,3 @@
+#!/bin/bash  -ex
+env $(cat env2 | xargs) ./scripts/sendit.sh
+env $(cat env2 | xargs) ansible-events github-ci-cd-rules.yml -i inventory.yml -S sources --env-vars webhook,connection_str,token,queue_name ${1}
