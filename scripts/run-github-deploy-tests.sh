59126c1ccb2fac412bdbee14d4ffe1f09e2adac8
Ben Thomasson <bthomass@redhat.com>
2022-03-08 11:04:41 -0500

    Fixes github integration for prs

--- scripts/run-github-deploy-tests.sh
+++ scripts/run-github-deploy-tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash  -ex
 env $(cat env2 | xargs) ./scripts/sendit.sh
-env $(cat env2 | xargs) ansible-events github-ci-cd-rules.yml -i inventory.yml -S sources --env-vars webhook,connection_str,token,queue_name,repo_name ${1}
+env $(cat env2 | xargs) ansible-events github-ci-cd-rules.yml -i inventory.yml -S sources --env-vars webhook,connection_str,token,queue_name,repo_name,gh_user,gh_token ${1}
